c189c758a6db096062bf55d892ac097e7889ccc9
Yasmine Naas <ch091976@ens.uqam.ca>
2024-03-08 13:44:27 -0500

    ajout validation taille plateau et init jeu

--- include/jeu.h
+++ include/jeu.h
@@ -20,7 +20,7 @@ typedef struct {
 
 
 void commencer_jeu(void);
-void terminer_jeu(int shots);
+void terminer_jeu(int tirs);
 
 char **allouer_plateau(int taille);
 void free_plateau(char **plateau, int taille);
@@ -28,6 +28,11 @@ int verifier_placement(char **plateau, Case debut, int taille, int direction, in
 void placer_navire(char **plateau, Navire *navire, int taille_plateau);
 void affichage_plateau(char **plateau, int taille);
 void touche_navire(Navire *navire, int x, int y, int *navire_coule);
+
+int valider_taille_plateau(void);
+void initialiser_jeu(int taille, char ***plateau, char ***action_plateau, Navire navires[]);
+int get_coordonnee_tir(int *x, int *y, int taille);
+void proceder_tir(char **plateau, char **action_plateau, Navire navires[], int x, int y, int *tirs, int *navire_coule);
 
 
 #endif 
