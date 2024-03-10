239a1c864318b9745a91bd5d1f5555382d1dd130
Yasmine Naas <ch091976@ens.uqam.ca>
2024-03-10 14:08:58 -0400

    ajout fonctions pour gerer erreur de saisie de coordonnees

--- include/jeu.h
+++ include/jeu.h
@@ -1,6 +1,8 @@
 #ifndef JEU_H
 #define JEU_H
 
+#include "stats.h"
+
 #define TAILLE_MIN_PLATEAU 6
 #define TAILLE_MAX_PLATEAU 100
 
@@ -14,7 +16,7 @@ typedef struct {
 typedef struct {
     Case positions[5];
     int taille;
-    char *nom;
+    char *nom;    
     int touche;
 } Navire;
 
@@ -27,15 +29,19 @@ void free_plateau(char **plateau, int taille);
 int verifier_placement(char **plateau, Case debut, int taille, int direction, int taille_plateau);
 void placer_navire(char **plateau, Navire *navire, int taille_plateau);
 void affichage_plateau(char **plateau, int taille);
-void touche_navire(Navire *navire, int x, int y, int *navire_coule);
+void touche_navire(Navire *navire, int x, int y, int *navire_coule, GameStats *stats);
 
 int valider_taille_plateau(void);
 void initialiser_jeu(int taille, char ***plateau, char ***action_plateau, Navire navires[]);
+void nettoyer_stdin(void);
+int lire_coordonnees(char *input, int *x, int *y);
+int verifier_coordonnees(int x, int y, int taille);
+int lire_entree_utilisateur(int *x, int *y, int taille);
 int get_coordonnee_tir(int *x, int *y, int taille);
-void proceder_tir(char **plateau, char **action_plateau, Navire navires[], int x, int y, int *tirs, int *navire_coule);
+void proceder_tir(char **plateau, char **action_plateau, Navire navires[], int x, int y, int *tirs, int *navire_coule, GameStats *stats);
 
-void jouer_jeu(char **plateau, char **action_plateau, Navire navires[], int taille);
-void debut_jeu(void);
+void jouer_jeu(char **plateau, char **action_plateau, Navire navires[], int taille, GameStats *stats);
+void debut_jeu(GameStats *stats);
 
 
 #endif 
