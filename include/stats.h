e604340a29bd3a2e11324356c5b5bb1aa1d84f23
Yasmine Naas <ch091976@ens.uqam.ca>
2024-03-10 16:54:44 -0400

    Ajout de fonctions pour tenir compte des stats du jeu

--- include/stats.h
+++ include/stats.h
@@ -1,7 +1,16 @@
-#ifndef STATISTIQUES_H
-#define STATISTIQUES_H
+#ifndef STATS_H
+#define STATS_H
 
-void init_statistiques();
-void enregistrer_statistiques(const char *nom_fichier);
+typedef struct {
+    int nbr_total_tirs;     
+    int uniqueLetters;       
+    int coups_eau;      
+    int deja_joue;         
+    int coups_touche;            
+    char dernier_navire[20];    
+} GameStats;
 
-#endif 
+void init_stats(GameStats *stats);
+void write_stats_file(const GameStats *stats, const char *nom_fichier);
+
+#endif
--- stats.c
+++ stats.c
@@ -1,2 +1,28 @@
 #include "include/stats.h"
 #include <stdio.h>
+#include <string.h>
+
+void init_stats(GameStats *stats) {
+    stats->nbr_total_tirs = 0;
+    stats->uniqueLetters = 0;
+    stats->coups_eau = 0;
+    stats->deja_joue = 0;
+    stats->coups_touche = 0;
+    memset(stats->dernier_navire, 0, sizeof(stats->dernier_navire));     
+}
+
+
+void write_stats_file(const GameStats *stats, const char *nom_fichier) {
+    FILE *file = fopen(nom_fichier, "w");
+    if (file) {
+        fprintf(file, "Nombre total de coups: %d\n", stats->nbr_total_tirs);
+        fprintf(file, "Nombre de lettres sans doublon du premier navire touché: %d\n", stats->uniqueLetters);
+        fprintf(file, "Nombre total de coups à l’eau: %d\n", stats->coups_eau);
+        fprintf(file, "Nombre total de coups déjà joué: %d\n", stats->deja_joue);
+        fprintf(file, "Nombre total de coups touché: %d\n", stats->coups_touche);
+        fprintf(file, "Nom du dernier navire coulé: %s\n", stats->dernier_navire);
+        fclose(file);
+    } else {
+        printf("Erreur lors de l'ouverture du fichier '%s'\n", nom_fichier);
+    }
+}
